e539b0305c79519b7b684637f4d955eebadb8ada
2ma1995 <vthquddbsv@gmail.com>
2024-03-21 23:06:13 +0900

    2024.03.21 evemtlistener(-ing이해가 아직안됌)

--- js_dom/app.js
+++ js_dom/app.js
@@ -1,33 +1,19 @@
-// console.log(window.document);
-// console.dir(window.document); //더자세하게 나옴
+// eventlistener
+let paragraphElement = document.querySelector("p");
 
-// document.body.children[1].children[0].href = "https://google.com";
+function changeParagraphText() {
+  paragraphElement.textContent = "Clicked";
+  console.log("Paragraph clicked!");
+}
 
-let anchorElement = document.getElementById("external-link");
-anchorElement.href = "https://google.com";
+paragraphElement.addEventListener("click", changeParagraphText);
 
-anchorElement = document.querySelector("p a");
-anchorElement.href = "https://naver.com";
+// ======input관련
+let inputElement = document.querySelector("input");
 
-// add an element
-// 1.create the new element
-let newAnchorElement = document.createElement("a");
-newAnchorElement.href = "https://naver.com";
-newAnchorElement.textContent = "This lead to naver";
-// 2. get access to the parent element that should hold the new element
-let firstParagraph = document.querySelector("p");
-// 3.insert the new element into the parent element content
-firstParagraph.append(newAnchorElement);
+function retrieveUserInput() {
+  let enteredText = inputElement.value;
+  console.log(enteredText);
+}
 
-//remove element
-
-//1.select the element that shoul;d be removed
-let firstH1Element = document.querySelector("h1");
-// 2.remove it
-firstH1Element.remove();
-// 다른 방법
-firstH1Element.parentElement.removeChild(firstH1Element); // for the order browsers
-
-// move elements
-firstParagraph.parentElement.append(firstParagraph);
-// 추가해줘야하는 장소에 부모 요소를 선택한 다음, append or insert를 사용해 이동시켜준다.
+inputElement.addEventListener("input", retrieveUserInput);
